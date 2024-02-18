36547cb03512026048250975a82478a0d1bf2fa0
KZTmm <kzt588466@gmail.com>
2024-02-18 20:42:40 +0630

    adding area file

---
+++ area.py
@@ -0,0 +1,7 @@
+def calculate_area(length,width):
+    return length * width
+length=float(input("Enter the lenght of the rectangle: "))
+width=float(input("Enter the width of the rectangle: "))
+
+area=calculate_area(5,4)
+print("Area of the rectangle:",area)
